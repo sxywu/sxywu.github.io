11fd8857a62dbc7f32a7a0ebe650ded2e3723822
Shirley Wu <shirleyxueyangwu@gmail.com>
2016-06-16 21:04:02 -0700

    starting bundle

---
+++ scripts/components/App.jsx
@@ -0,0 +1,13 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var _ = require('lodash');
+
+var App = React.createClass({
+  render() {
+    return (
+      <div />
+    );
+  }
+});
+
+module.exports = App;
