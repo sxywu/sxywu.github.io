022a78f293f301cdd46a4b6eeb5c2649bf8655d4
Shirley Wu <shirleyxueyangwu@gmail.com>
2016-06-22 21:54:26 -0700

    remove tech info from header

--- scripts/components/Header.jsx
+++ scripts/components/Header.jsx
@@ -14,10 +14,8 @@ var Header = React.createClass({
 Hi, my name is Shirley.
         </h1>
         <h2>
-I'm a software engineer based in the San Francisco Bay Area, and I like to <Label data={this.props.labels.build} onFilter={this.props.onFilter}>build</Label> data visualizations for learning and for fun.
-I <Label data={this.props.labels.write} onFilter={this.props.onFilter}>write</Label> once in a while about the things I learn, and give <Label data={this.props.labels.talk} onFilter={this.props.onFilter}>talks</Label> once in a while too.
-I primarily use <Label data={this.props.labels.d3} onFilter={this.props.onFilter}>D3.js</Label> and <Label data={this.props.labels.react} onFilter={this.props.onFilter}>React.js</Label> in my work, and used to use <Label data={this.props.labels.backbone} onFilter={this.props.onFilter}>Backbone.js</Label> a lot.
-I've also dabbled with <Label data={this.props.labels.ror} onFilter={this.props.onFilter}>Ruby on Rails</Label> and <Label data={this.props.labels.node} onFilter={this.props.onFilter}>Node</Label>, and am currently learning Redux.
+I'm a software engineer based in the San Francisco Bay Area, and I like to <Label data={this.props.labels.build} onFilter={this.props.onFilter}>build</Label> web applications centered around data visualizations.
+I <Label data={this.props.labels.write} onFilter={this.props.onFilter}>write</Label> once in a while about the things I learn, and give <Label data={this.props.labels.talk} onFilter={this.props.onFilter}>talks</Label> once in a while too.  I help <Label data={this.props.labels.organize} onFilter={this.props.onFilter}>organize</Label> meetups for the <a href='www.meetup.com/Bay-Area-d3-User-Group/' target='_new'>Bay Area D3 User Group</a>, and the annual <a href='http://visfest.com/d3unconf/' target='_new'>d3.unconf</a>.
         </h2>
         <h2>
 I experiment on <a href='http://bl.ocks.org/sxywu' target='_new'>bl.ocks</a>, version control with <a href='https://github.com/sxywu' target='_new'>github</a>, write on <a href='https://medium.com/@sxywu' target='_new'>medium</a>, and generate 140 characters or less on <a href='https://twitter.com/sxywu' target='_new'>twitter</a>.
